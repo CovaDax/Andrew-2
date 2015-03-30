0d8270abc4e4867e120e799f2e70ab13f663fbc3
ajmeyers <ameyers.central@gmail.com>
2015-03-30 10:48:13 -0400

    Changes were made

---
+++ src/test/java/edu/fgcu/Cycle_Detection/A.java
@@ -0,0 +1,11 @@
+package edu.fgcu.Cycle_Detection;
+
+
+public class A {
+	
+	private B b;
+
+	public A(){
+		this.b = new B();
+	}
+}
---
+++ src/test/java/edu/fgcu/Cycle_Detection/C.java
@@ -0,0 +1,10 @@
+package edu.fgcu.Cycle_Detection;
+
+public class C {
+
+	private A a;
+
+	public C(){
+		this.a = new A();
+	}
+}
