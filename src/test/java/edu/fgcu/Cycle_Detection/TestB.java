7494f47165e43bcf88de6036903bff53e4cc6334
Andrew Gari <covadax.software@gmail.com>
2015-04-02 05:14:54 -0400

    Tested All Classes

--- src/test/java/edu/fgcu/Cycle_Detection/TestB.java
+++ src/test/java/edu/fgcu/Cycle_Detection/TestB.java
@@ -1,5 +1,22 @@
 package edu.fgcu.Cycle_Detection;
 
-public class TestB {
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestB {
+	private B b;
+	
+	@Before
+	public void setup(){
+		b = new B();
+	}
+	
+	@Test
+	public void testCCon(){
+		C c = new C();
+		b.setC(c);
+		assertEquals(b.getC(),c);
+	}
 }
--- src/test/java/edu/fgcu/Cycle_Detection/TestC.java
+++ src/test/java/edu/fgcu/Cycle_Detection/TestC.java
@@ -1,5 +1,22 @@
 package edu.fgcu.Cycle_Detection;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestC {
+	private C c;
 	
+	@Before
+	public void setup(){
+		c = new C();
+	}
+	
+	@Test
+	public void testACon(){
+		A a = new A();
+		c.setA(a);
+		assertEquals(c.getA(),a);
+	}
 }
