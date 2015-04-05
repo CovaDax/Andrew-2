fdaf3c376f8d57243ad1e7d7af49b1dd0e0f1fc8
Andrew Gari <covadax.software@gmail.com>
2015-04-05 16:47:28 -0400

    treeMade

--- src/test/java/edu/fgcu/Cycle_Detection/Cycle_Tree_Test.java
+++ src/test/java/edu/fgcu/Cycle_Detection/Cycle_Tree_Test.java
@@ -1,6 +1,8 @@
 package edu.fgcu.Cycle_Detection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Random;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,6 +13,14 @@ public class Cycle_Tree_Test {
 	@Before
 	public void setup(){
 		tree = new Cycle_Tree(new A(0));
+	}
+	
+	@Test
+	public void testTreeMade(){
+		int rand = new Random().nextInt();
+		tree.createTree(rand);
+		assertNotNull(tree.getHead().getNext());
+		assertNotNull(tree.getHead().getLast());
 	}
 	
 	@Test
