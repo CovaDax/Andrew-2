e482a4106723cdcc60233d725a64d3947ba9357b
ajmeyers <ameyers.central@gmail.com>
2015-04-06 02:04:04 -0400

    first commit

---
+++ src/main/java/edu/fgcu/Cycle_Detection/CycleDetector.java
@@ -0,0 +1,28 @@
+package edu.fgcu.Cycle_Detection;
+
+public class CycleDetector {
+	
+
+	public CycleDetector() {
+		
+		
+	}
+	
+	public void DetectCycle() {
+		
+		
+	}
+	
+	
+	public void isCycle() {
+		
+		
+	}
+	
+	public static void main(String [] args){
+		
+		System.out.println("This is a Cycle Detection program!!!");
+		
+	}
+		
+}
