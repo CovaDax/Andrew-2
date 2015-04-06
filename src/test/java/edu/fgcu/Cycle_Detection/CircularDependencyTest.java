3609c4e20566830bbca1fedecb39beb281d8959b
ajmeyers <ameyers.central@gmail.com>
2015-04-06 11:39:18 -0400

    Added tests

---
+++ src/test/java/edu/fgcu/Cycle_Detection/CircularDependencyTest.java
@@ -0,0 +1,15 @@
+package edu.fgcu.Cycle_Detection;
+
+import org.junit.Test;
+
+public class CircularDependencyTest {
+
+	@Test(expected = RuntimeException.class)
+	public void testIsCircularDependency() {
+		
+		 
+	}
+	
+
+		
+}
