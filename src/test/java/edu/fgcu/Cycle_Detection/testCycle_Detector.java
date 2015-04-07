1ccd4756ff77692c426cc69483d582fbcad4fdf2
Andrew Gari <covadax.software@gmail.com>
2015-04-07 00:07:37 -0400

    test

---
+++ src/test/java/edu/fgcu/Cycle_Detection/testCycle_Detector.java
@@ -0,0 +1,12 @@
+package edu.fgcu.Cycle_Detection;
+
+import org.junit.Test;
+
+public class testCycle_Detector {
+	
+	@Test (expected = RuntimeException.class)
+	public void testNothing(){
+		throw new RuntimeException("Test");
+	}
+	
+}
