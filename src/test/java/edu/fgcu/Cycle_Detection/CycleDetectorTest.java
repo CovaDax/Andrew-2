d410f2d7e5378481b9ccae10dfa1136034ad5942
ajmeyers <ameyers.central@gmail.com>
2015-04-08 19:41:23 -0400

    Tests now fail

--- src/test/java/edu/fgcu/Cycle_Detection/CycleDetectorTest.java
+++ src/test/java/edu/fgcu/Cycle_Detection/CycleDetectorTest.java
@@ -15,7 +15,7 @@ public class CycleDetectorTest {
 	@Test(expected = RuntimeException.class)
 	public void testFindCycles() {
 		
-		throw new RuntimeException("msg");
+		//throw new RuntimeException("msg");
 	}
 	
 	@Test(expected = RuntimeException.class)
