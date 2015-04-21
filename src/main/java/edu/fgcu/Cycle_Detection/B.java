c81f74042a41bdada270a0bcb92fafe3235cc492
Andrew Gari <covadax.software@gmail.com>
2015-04-21 17:25:02 -0400

    Polish

--- src/main/java/edu/fgcu/Cycle_Detection/B.java
+++ src/main/java/edu/fgcu/Cycle_Detection/B.java
@@ -3,5 +3,5 @@ package edu.fgcu.Cycle_Detection;
 
 public class B {
 	private C c;
-//	private A a;
+	private A a;
 }
--- src/main/java/edu/fgcu/Cycle_Detection/C.java
+++ src/main/java/edu/fgcu/Cycle_Detection/C.java
@@ -3,5 +3,4 @@ package edu.fgcu.Cycle_Detection;
 
 public class C {
 	private A a;
-	private C c;
 }
