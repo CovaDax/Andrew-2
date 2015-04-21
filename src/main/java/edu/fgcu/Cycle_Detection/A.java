dec921e8bf9a55495ac9c354cf1b453a0ce310e4
Andrew Gari <covadax.software@gmail.com>
2015-04-21 17:45:39 -0400

    dir

--- src/main/java/edu/fgcu/Cycle_Detection/A.java
+++ src/main/java/edu/fgcu/Cycle_Detection/A.java
@@ -1,6 +1,5 @@
 package edu.fgcu.Cycle_Detection;
 
-
 public class A {
 	private A a;
 	private B b;
--- src/test/java/edu/fgcu/Cycle_Detection/A.java
+++
@@ -1,7 +0,0 @@
-package edu.fgcu.Cycle_Detection;
-
-
-public class A {
-	private A a;
-	private B b;
-}
